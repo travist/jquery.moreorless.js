c0f0d52bbfc193874b89e9bfc0abfe185a642190
Travis Tidwell <travis@allplayers.com>
2011-12-21 17:29:09 +0000

    Completely refactored how this widget works.

--- moreorless.js
+++ moreorless.js
@@ -14,7 +14,7 @@
  *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  *  copies of the Software, and to permit persons to whom the Software is
  *  furnished to do so, subject to the following conditions:
- *  
+ *
  *  The above copyright notice and this permission notice shall be included in
  *  all copies or substantial portions of the Software.
 
@@ -28,55 +28,102 @@
  */
 (function($) {
   jQuery.fn.moreorless = function( min_height, more_text, less_text ) {
-  
-    var read_more = "<strong><i><u>" + more_text + "</u></i></strong>";
-    var read_less = "<strong><i><u>" + less_text + "</u></i></strong>";
-  
-    // Expand or contract a div element.
-    function expandDiv( link, element, div_height ) {
-      if( !$(element).hasClass('expanded') ) {
-        $(link).html(read_less);
-        $(element).addClass('expanded').animate({
-          height:div_height
-        }, 200);
-      }
-      else {
-        $(link).html(read_more);
-        $(element).removeClass('expanded').animate({
-          height:min_height
-        }, 200);
-      }
-    }
+
+    // Default the parameters.
+    min_height = min_height || 100;
+    more_text = more_text || "more";
+    less_text = less_text || "less";
 
     // Iterate over each element.
     this.each( function() {
-    
-      // Wrap the element in div tags.
-      var element = this;
-     
-      // Add overflow hidden to the div wrappers.
-      $(element).css({
-        overflow: 'hidden'
-      });
-      
-      // Get the div height.
-      var div_height = $(element).height();
-      
-      // If the div height is greater than the min height.
-      if (div_height > min_height) {
-      
-        // Set the div wrapper height.
-        $(element).height(min_height);
-        
-        // Add the "more" link and bind to the click event.
-        $(element).after('<a href="#" class="expanding_div">' + read_more + '</a><br/>').next().bind('click', {div:element}, function(event) {
-          
-          // Prevent default, and then call our own handler.
-          event.preventDefault();
-          event.stopPropagation();
-          expandDiv(this, event.data.div, div_height);
-        });
+
+      // Define all the elements of interest.
+      var element = $(this);
+      var div_height = element.height();
+      var wrapper = element.wrap('<div></div>').parent();
+      wrapper.addClass('moreorless_wrapper');
+      var link = $('a.moreorless_link');
+      var read_more = "<i><u>" + more_text + "</u></i>";
+      var read_less = "<i><u>" + less_text + "</u></i>";
+
+      // If the link is not found create it.
+      if (link.length == 0) {
+        link = $(document.createElement('a'));
+        link.attr('href', 'javascript:void(0)');
+        link.addClass('moreorless_link');
+        link.append(read_more);
       }
+
+      function bindLink() {
+        if (link.length > 0) {
+          // Bind to when the link is clicked.
+          link.unbind().bind('click', function(event) {
+            event.preventDefault();
+            event.stopPropagation();
+            if( wrapper.hasClass('expanded') ) {
+              link.html(read_more);
+              wrapper.removeClass('expanded').animate({
+                height:min_height
+              }, 200);
+            }
+            else {
+              link.html(read_less);
+              wrapper.addClass('expanded').animate({
+                height:div_height
+              }, 200);
+            }
+          });
+        }
+      }
+
+      // Check the min or less height.
+      function checkHeight() {
+        if (div_height > min_height) {
+          bindLink();
+          wrapper.css({overflow: 'hidden'}).height(min_height).after(link);
+        }
+        else {
+          wrapper.css({overflow: null}).height(null).next('a.moreorless_link').remove();
+        }
+      }
+
+      // Create a function to set the new height of the element.
+      function setElementHeight() {
+        div_height = element.height();
+        checkHeight();
+      }
+
+      // Bind to when the link is clicked.
+      link.bind('click', function(event) {
+        event.preventDefault();
+        event.stopPropagation();
+        if( wrapper.hasClass('expanded') ) {
+          link.html(read_more);
+          wrapper.removeClass('expanded').animate({
+            height:min_height
+          }, 200);
+        }
+        else {
+          link.html(read_less);
+          wrapper.addClass('expanded').animate({
+            height:div_height
+          }, 200);
+        }
+      });
+
+      // Check the height.
+      checkHeight();
+
+      // Trigger when resize events occur, but don't trigger to fast.
+      var resizeTimer = 0;
+      $(window).bind('resize', function () {
+        clearTimeout(resizeTimer);
+        resizeTimer = setTimeout(setElementHeight, 100);
+      });
+      element.bind('resize', function() {
+        clearTimeout(resizeTimer);
+        resizeTimer = setTimeout(setElementHeight, 100);
+      });
     });
   }
 })(jQuery);
